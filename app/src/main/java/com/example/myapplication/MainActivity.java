391fab14eb6f89b0c468b7ec3fbd4afc656afc96
wls1234 <1944800323@qq.com>
2019-03-27 22:16:55 +0800

    first test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView out1;
+    TextView out;
+   EditText edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second);
+        TextView out1=findViewById(R.id.textView2);
+       EditText edit=findViewById(R.id.tem1);
+       Button btn=findViewById(R.id.button);
+       TextView out=findViewById(R.id.textView3);
+        btn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        String str=edit.getText().toString();
+        float tem1=Float.parseFloat(str);
+        float tem=(float)(tem1*(9/5)+32);
+        out.setText("结果："+tem);
     }
 }
+
+
+
+
